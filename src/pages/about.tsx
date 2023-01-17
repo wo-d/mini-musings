b3b5a75732509723a9e2ab235433915e41fe1869
William O'Donnell <william.e.odonnell@gmail.com>
2023-01-17 18:23:36 -0500

    updating about page and fixing dependency vulnerabilities

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,7 +6,19 @@ import Seo from '../components/seo';
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Blah blah introduction stuff</p>
+      <p>
+        Hello and welcome to my blog! Within these pages you'll find my journey
+        of the <i>Hobby</i>. Everything from painting to wargaming to who knows
+        what else.
+      </p>
+      <p>
+        I've been active in this hobby since ~2021 with my introduction being
+        innocuously, painting a Kill Team. A friend had already built some Space
+        Marines, and with no ulterior motive at all asked if I'd like to play.
+        I've been hooked ever since. I mostly stick to 40K and have at least
+        2000 points of both Drukhari and Blood Angels. I enjoy the painting side
+        of things and do one off models on occasion.
+      </p>
     </Layout>
   );
 };
